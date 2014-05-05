df47badb19f52278ff0fcff27ce3ec0cad460b96
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2014-05-05 14:32:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# coding=utf-8
 import webapp2
+from google.appengine.api.search import search
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+class PageHandler(webapp2.RequestHandler):
+  def get(self):
+    messages = []
+    
+    index = search.Index(name = 'DeleteMe')
+    language = None
+    fields = [
+      search.AtomField(name = 'tag', value = 'zamówienia', language = language),
+    ]
+    document = search.Document(doc_id = 'deleteMe1', fields = fields, language = language)
+    index.put(document)
+    document = search.Document(doc_id = 'deleteMe2', fields = fields, language = language)
+    index.put(document)
+    
+    queryString = ''
+    queryOptions = search.QueryOptions(cursor = search.Cursor(), limit = 1)
+    searchQuery = search.Query(queryString, options = queryOptions)
+    results = index.search(searchQuery)
+    messages.append(u'query = %s, hits = %s'
+                    % (queryString, results.number_found))
+    messages.append(dir(results))
+    messages.append(results.results)
+    messages.append(results.cursor)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    queryString = ''
+    queryOptions = search.QueryOptions(cursor = search.Cursor(), limit = 3)
+    searchQuery = search.Query(queryString, options = queryOptions)
+    results = index.search(searchQuery)
+    messages.append(u'query = %s, hits = %s'
+                    % (queryString, results.number_found))
+    messages.append(dir(results))
+    messages.append(results.results)
+    messages.append(results.cursor)
+
+    self.response.out.write(u'<pre>')    
+    self.response.out.write(u'\n'.join(unicode(x) for x in messages))
+    self.response.out.write(u'</pre')
+
+
+APP = webapp2.WSGIApplication([
+    ('/.*', PageHandler),
 ], debug=True)
